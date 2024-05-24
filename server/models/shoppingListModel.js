05ba9f1ea211ae5b8844bfa5659e8e7ec3ebc513
YellowSar <hsgopoke@gmail.com>
2024-05-24 17:51:58 +0300

    done

---
+++ server/models/shoppingListModel.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    return sequelize.define(
+        'ShoppingList',
+        {
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            price: {
+                type: DataTypes.SMALLINT,
+                allowNull: false,
+            },
+            date: {
+                type: DataTypes.DATE,
+                // allowNull: false,
+            },
+            isPurchased: {
+                type: DataTypes.BOOLEAN,
+            },
+            important: {
+                type: DataTypes.BOOLEAN,
+            },
+            userID: {
+                type: DataTypes.SMALLINT,
+            },
+        },
+        {
+            timestamps: false,
+        },
+    );
+};
